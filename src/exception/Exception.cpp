ea177ba71b7d0d3b2ce207206115ded0a5993230
AomaYple <2890599214@qq.com>
2024-10-30 18:47:49 +0800

    为Exception构造函数添加重载

--- src/exception/Exception.cpp
+++ src/exception/Exception.cpp
@@ -1,5 +1,9 @@
 #include "Exception.hpp"
 
+coRing::Exception::Exception(const char *const message) noexcept : message{message} {}
+
+coRing::Exception::Exception(const std::string_view message) noexcept : message{message} {}
+
 coRing::Exception::Exception(std::string &&message) noexcept : message{std::move(message)} {}
 
 auto coRing::Exception::what() const noexcept -> const char * { return this->message.c_str(); };
--- src/exception/Exception.hpp
+++ src/exception/Exception.hpp
@@ -6,6 +6,10 @@
 namespace coRing {
     class Exception final : public std::exception {
     public:
+        explicit Exception(const char *message) noexcept;
+
+        explicit Exception(std::string_view message) noexcept;
+
         explicit Exception(std::string &&message) noexcept;
 
         [[nodiscard]] auto what() const noexcept -> const char * override;
