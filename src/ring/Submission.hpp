9b4687d73cd4020195ba6a21b88b0e1a08ce9af5
AomaYple <2890599214@qq.com>
2024-11-01 09:07:26 +0800

    Submission类放在coContext里

--- src/ring/Submission.hpp
+++ src/ring/Submission.hpp
@@ -2,14 +2,16 @@
 
 #include <variant>
 
-struct Submission {
-    enum class Type : unsigned char { close };
+namespace coContext {
+    struct Submission {
+        enum class Type : unsigned char { close };
 
-    struct Close {};
+        struct Close {};
 
-    int fileDescriptor;
-    unsigned int flags;
-    unsigned short ioPriority;
-    unsigned long userData;
-    std::variant<Close> parameter;
-};
+        int fileDescriptor;
+        unsigned int flags;
+        unsigned short ioPriority;
+        unsigned long userData;
+        std::variant<Close> parameter;
+    };
+}    // namespace coContext
