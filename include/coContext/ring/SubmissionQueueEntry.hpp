4f331ba356ce214f64da26b7b109bc3500d32b26
AomaYple <2890599214@qq.com>
2024-12-18 14:20:12 +0800

    将getFileStatus的buffer参数的类型由引用改为指针

--- include/coContext/ring/SubmissionQueueEntry.hpp
+++ include/coContext/ring/SubmissionQueueEntry.hpp
@@ -118,7 +118,7 @@ namespace coContext {
                           std::uint64_t length) const noexcept -> void;
 
         auto getFileStatus(std::int32_t directoryFileDescriptor, std::string_view path, std::int32_t flags,
-                           std::uint32_t mask, struct statx &buffer) const noexcept -> void;
+                           std::uint32_t mask, struct statx *buffer) const noexcept -> void;
 
         auto getExtendedAttribute(std::string_view path, std::string_view name, std::span<char> value) const noexcept
             -> void;
