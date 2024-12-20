e7b34570972be40eb856eedb658da9130487d5d5
AomaYple <2890599214@qq.com>
2024-12-20 09:52:27 +0800

    debug模式不使用mimalloc，防止与asan冲突

--- src/memory/memoryResource.cpp
+++ src/memory/memoryResource.cpp
@@ -3,8 +3,12 @@
 #include "MiMallocResource.hpp"
 
 auto coContext::getMemoryResource() -> std::pmr::memory_resource * {
+#ifndef NDEBUG
+    thread_local std::pmr::unsynchronized_pool_resource resource;
+#else
     static constinit MiMallocResource miMallocResource;
     thread_local std::pmr::unsynchronized_pool_resource resource{std::addressof(miMallocResource)};
+#endif
 
     return std::addressof(resource);
 }
