134d161d96a148122aa2f744f28ce8ebb2332fb3
AomaYple <2890599214@qq.com>
2024-12-20 10:37:15 +0800

    size()改std::size

--- src/ring/SubmissionQueueEntry.cpp
+++ src/ring/SubmissionQueueEntry.cpp
@@ -267,25 +267,25 @@ auto coContext::SubmissionQueueEntry::getFileStatus(const std::int32_t directory
 
 auto coContext::SubmissionQueueEntry::getExtendedAttribute(const std::string_view path, const std::string_view name,
                                                            const std::span<char> value) const noexcept -> void {
-    io_uring_prep_getxattr(this->handle, std::data(name), std::data(value), std::data(path), value.size());
+    io_uring_prep_getxattr(this->handle, std::data(name), std::data(value), std::data(path), std::size(value));
 }
 
 auto coContext::SubmissionQueueEntry::getExtendedAttribute(const std::int32_t fileDescriptor,
                                                            const std::string_view name,
                                                            const std::span<char> value) const noexcept -> void {
-    io_uring_prep_fgetxattr(this->handle, fileDescriptor, std::data(name), std::data(value), value.size());
+    io_uring_prep_fgetxattr(this->handle, fileDescriptor, std::data(name), std::data(value), std::size(value));
 }
 
 auto coContext::SubmissionQueueEntry::setExtendedAttribute(const std::string_view path, const std::string_view name,
                                                            const std::span<char> value,
                                                            const std::int32_t flags) const noexcept -> void {
-    io_uring_prep_setxattr(this->handle, std::data(name), std::data(value), std::data(path), flags, value.size());
+    io_uring_prep_setxattr(this->handle, std::data(name), std::data(value), std::data(path), flags, std::size(value));
 }
 
 auto coContext::SubmissionQueueEntry::setExtendedAttribute(const std::int32_t fileDescriptor,
                                                            const std::string_view name, const std::span<char> value,
                                                            const std::int32_t flags) const noexcept -> void {
-    io_uring_prep_fsetxattr(this->handle, fileDescriptor, std::data(name), std::data(value), flags, value.size());
+    io_uring_prep_fsetxattr(this->handle, fileDescriptor, std::data(name), std::data(value), flags, std::size(value));
 }
 
 auto coContext::SubmissionQueueEntry::makeDirectory(const std::string_view path, const mode_t mode) const noexcept
