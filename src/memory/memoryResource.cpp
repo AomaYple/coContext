408acc2f43de6df884ac7481ef8c3a168944d6ae
AomaYple <2890599214@qq.com>
2025-01-02 09:58:04 +0800

    只在Release模式下使用mimalloc

--- src/memory/memoryResource.cpp
+++ src/memory/memoryResource.cpp
@@ -1,14 +1,16 @@
 #include "memoryResource.hpp"
 
-#include "MiMallocResource.hpp"
+#ifdef NDEBUG
+    #include "MiMallocResource.hpp"
+#endif    // NDEBUG
 
 auto coContext::internal::getMemoryResource() -> std::pmr::memory_resource * {
 #ifndef NDEBUG
     thread_local std::pmr::unsynchronized_pool_resource resource;
-#else
+#else     // NDEBUG
     static constinit MiMallocResource miMallocResource;
     thread_local std::pmr::unsynchronized_pool_resource resource{std::addressof(miMallocResource)};
-#endif
+#endif    // NDEBUG
 
     return std::addressof(resource);
 }
