fbe27c51d33ab2575831e8643136cb1643e0c1a6
AomaYple <2890599214@qq.com>
2025-02-06 17:00:38 +0800

    更新README

--- README.md
+++ README.md
@@ -24,20 +24,20 @@
 #include <coContext/coContext.hpp> 
 ```
 
-简单示例，向调度器添加`func`协程，启动调度器；`func`协程发起`close`操作
+简单示例，向调度器添加`func`协程，启动调度器；`func`协程发起`noOperation`操作
 
 ```c++
 #include <coContext/coContext.hpp>
 
 [[nodiscard]] auto func() -> coContext::Task<> {    // Task模板参数为<>，表示该协程不返回任何值
-    co_await coContext::close(-1);    // 发起close操作
+    co_await coContext::noOperation();    // 发起close操作
     // co_return; 无返回值的协程可以省略co_return语句
 }
 
 auto main() -> int {
-    spawn(func);         // 将func函数作为协程任务加入到协程调度器中
+    spawn(func);    // 将func函数作为协程任务加入到协程调度器中
 
-    coContext::run();    //启动调度器
+    coContext::run();    // 启动调度器
 }
 ```
 
--- example/spawnReturnValue.cpp
+++ example/spawnReturnValue.cpp
@@ -1,13 +1,12 @@
 #include <coContext/coContext.hpp>
-#include <print>
 
-using namespace std::string_view_literals;
-
-[[nodiscard]] auto function() -> coContext::Task<std::int32_t> { co_return co_await coContext::noOperation(); }
+[[nodiscard]] auto func() -> coContext::Task<> {    // Task模板参数为<>，表示该协程不返回任何值
+    co_await coContext::noOperation();    // 发起close操作
+    // co_return; 无返回值的协程可以省略co_return语句
+}
 
 auto main() -> int {
-    coContext::SpawnResult result{spawn<std::int32_t>(function)};
-    const std::jthread worker{[&result] { std::println("{}"sv, result.value.get()); }};
+    spawn(func);    // 将func函数作为协程任务加入到协程调度器中
 
-    coContext::run();
+    coContext::run();    // 启动调度器
 }
