1f0df4bf2c512a4a476d138248e59a86c11abd12
AomaYple <2890599214@qq.com>
2025-02-07 14:12:42 +0800

    改进spawnReturnValue.cpp

--- example/spawnReturnValue.cpp
+++ example/spawnReturnValue.cpp
@@ -6,8 +6,8 @@ using namespace std::string_view_literals;
 [[nodiscard]] auto function() -> coContext::Task<std::int32_t> { co_return co_await coContext::noOperation(); }
 
 auto main() -> int {
-    coContext::SpawnResult result{spawn<std::int32_t>(function)};
-    const std::jthread worker{[&result] { std::println("spawn result: {}"sv, result.value.get()); }};
+    auto [value, taskId]{spawn<std::int32_t>(function)};
+    const std::jthread worker{[&value] { std::println("spawn result: {}"sv, value.get()); }};
 
     coContext::run();
 }
